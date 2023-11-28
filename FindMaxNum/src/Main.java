90a2e9f183688ccab403bec4abcfb338a0e8d728
Jeonghoonchoi74 <46080119+Jeonghoonchoi74@users.noreply.github.com>
2023-11-28 22:52:41 +0900

    Find max number

---
+++ FindMaxNum/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static int findMaximum(int[ ] A) {
+        int i, max;
+        max = A[0];
+        for (i = 1; i < A.length; i++) {
+            if (A[i] > max) {
+                max = A[i];
+            }
+        }
+        return max;
+    }
+
+
+    public static void main(String[] args) {
+        int[ ] 	intArray  =  {72, 60, 83, 47, 89, 95};
+        int maximum;
+        maximum =  findMaximum(intArray);
+        System.out.println("최댓값 = " + maximum);
+    }
+}
