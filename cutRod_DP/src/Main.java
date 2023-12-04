cf2eabeac8aad9f0b5c5e763f45b16c48c32e3f0
Jeonghoonchoi74 <46080119+Jeonghoonchoi74@users.noreply.github.com>
2023-12-04 20:15:01 +0900

    cutRod_DP 수정

--- cutRod_DP/src/Main.java
+++ cutRod_DP/src/Main.java
@@ -4,19 +4,19 @@ public class Main {
         int[] prices = {1, 5, 8, 9, 10, 17, 17, 20, 24, 30, 31, 32, 33, 34, 35, 36, 37, 38};
         int n = 14;
         int result = cutRod_DP(prices, n);
-        System.out.println(result);
+        System.out.println("Max sell value is: " + result);
     }
 
-    public static int cutRod_DP(int[] p, int n) {
-        int[] r = new int[n + 1];
-        r[0] = 0;
-        for (int j = 1; j <= n; j++) {
-            int q = Integer.MIN_VALUE;
-            for (int i = 1; i <= j; i++) {
-                q = Math.max(q, p[i] + r[j - i]);
+    public static int cutRod_DP(int[] prices, int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        for(int i = 1; i < n + 1; i++) {
+            int max = Integer.MIN_VALUE;
+            for(int j = 0; j < i; j++) {
+                max = Math.max(max, prices[j] + dp[i - j - 1]);
             }
-            r[j] = q;
+            dp[i] = max;
         }
-        return r[n];
+        return dp[n];
     }
 }
