cc64d5bc7178afd71db3f9f6fa3667d71488fb36
Jeonghoonchoi74 <46080119+Jeonghoonchoi74@users.noreply.github.com>
2023-12-04 20:11:07 +0900

    CutRod_DC 수정

--- cutRod_DC/src/Main.java
+++ cutRod_DC/src/Main.java
@@ -2,9 +2,8 @@ public class Main {
     public static void main(String[] args) {
         int[] prices = {1, 5, 8, 9, 10, 17, 17, 20, 24, 30, 31, 32, 33, 34, 35, 36, 37, 38};
         int n = 14;
-        int result = 0;
-        int maxSellValue = cutRod_DC(prices, n);
-        System.out.println("Max sell value is: " + maxSellValue);
+        int result = cutRod_DC(prices, n);
+        System.out.println("Max sell value is: " + result);
     }
 
     public static int cutRod_DC(int[] prices, int n) {
